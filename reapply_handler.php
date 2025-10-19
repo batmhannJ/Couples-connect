e7c7b450fe2a498100be1d2ad7f5c5171d5ca8ed
batmhannJ <reyeshannahjoy82@gmail.com>
2025-10-19 23:46:58 +0800

    chore: reapply process

--- reapply_handler.php
+++ reapply_handler.php
@@ -11,16 +11,37 @@ require_once('resources/connect4.php');
 $recid = $_POST['recid'];
 $ret = array();
 
-// Update the account status back to pending (FA) for reapplication
-$update_sql = "UPDATE mf_prog_users SET act_status = 'FA', remarks = 'Reapplied for approval' WHERE recid = ?";
-$stmt = $link->prepare($update_sql);
+// Get user information before deleting/archiving
+$select_sql = "SELECT userid, email FROM mf_prog_users WHERE recid = ?";
+$stmt_select = $link->prepare($select_sql);
+$stmt_select->execute([$recid]);
+$user_data = $stmt_select->fetch(PDO::FETCH_ASSOC);
 
-if ($stmt->execute([$recid])) {
-    $ret['status'] = true;
-    $ret['msg'] = 'Your reapplication has been submitted successfully. Please wait for admin approval.';
+if ($user_data) {
+    // Option 1: Delete the declined record completely
+    // This allows them to register fresh with the same email
+    $delete_sql = "DELETE FROM mf_prog_users WHERE recid = ?";
+    $stmt_delete = $link->prepare($delete_sql);
+    
+    if ($stmt_delete->execute([$recid])) {
+        // Also delete related records if any (couples info, etc.)
+        $delete_couples = "DELETE FROM ext_couples_accountinfo WHERE userid = ?";
+        $stmt_couples = $link->prepare($delete_couples);
+        $stmt_couples->execute([$user_data['userid']]);
+        
+        $ret['status'] = true;
+        $ret['msg'] = 'Your previous application has been cleared. You will be redirected to registration.';
+        $ret['redirect'] = true;
+        $ret['redirect_url'] = 'register_cc.php';
+    } else {
+        $ret['status'] = false;
+        $ret['msg'] = 'Failed to process reapplication. Please try again.';
+        $ret['redirect'] = false;
+    }
 } else {
     $ret['status'] = false;
-    $ret['msg'] = 'Failed to submit reapplication. Please try again.';
+    $ret['msg'] = 'User record not found.';
+    $ret['redirect'] = false;
 }
 
 header('Content-Type: application/json');
